4804fd48ae5f8ad0156fefaed8fde23e96f3a674
Kadeprina <166676171+Kadeprina@users.noreply.github.com>
2024-04-19 12:14:11 +0900

    Update notice.py

--- Project/travel_chat/pages/notice.py
+++ Project/travel_chat/pages/notice.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.header(f"공 지 사 항")
 st.text(f"챗봇이 말을 잘 안 들을 수 있어요! 아직 배우는 중이랍니다! 모두 응원해주세요!")
+st.text(f"길찾기는 한국은 지원 안 합니다. 구글이 지원 안 해요...")
+st.text(f"KeyError: 'places'는 구글 api측 오류입니다. 전달이 잘 되었으나 응답이 없는 상태입니다.(빈 응답 반환)")
